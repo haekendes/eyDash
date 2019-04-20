e626749186a1caa399ae93bfb1b5d1f8fbc5608d
haekendes <48943764+haekendes@users.noreply.github.com>
2019-04-20 23:58:19 +0200

    Add files via upload

---
+++ HibernateTest4/src/main/java/com/mycompany/hibernatetest4/NewMain.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hibernatetest4;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Robin Christ
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory
+                = Persistence.createEntityManagerFactory("com.mycompany_HibernateTest4_jar_1.0-SNAPSHOTPU");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+//        TestBitch c = new TestBitch();
+//        c.setHelloWorld("Hello World!");
+//        entityManager.persist(c);
+
+        Test t = new Test();
+        t.setName("Hello World!");
+        entityManager.persist(t);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+    
+}
