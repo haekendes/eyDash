8dff44b34ebf0cf955d0567926c752bcb4236358
haekendes <48943764+haekendes@users.noreply.github.com>
2019-04-22 10:53:00 +0200

    Add files via upload

---
+++ HibernateTest4/eyDash - createTables PostgreSQL.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE users(
+userid INTEGER,
+firstname TEXT,
+lastname TEXT,
+phonenumber INTEGER,
+PRIMARY KEY(userid, phonenumber)
+);
+
+CREATE TABLE appointments(
+userid INTEGER,
+appointmentid INTEGER,
+message TEXT,
+appointmentdate DATE,
+appointmenttime TIME,
+PRIMARY KEY(appointmentid)
+);
