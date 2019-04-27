19d4badc35b719f0dbf526de9ffced436313cbee
Haekendes <haekendes@gmx.net>
2019-04-27 15:16:53 +0200

    Initial commit

--- src/DisplayController/DisplayController.java
+++ src/DisplayController/DisplayController.java
@@ -12,49 +12,98 @@ import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  *
  * @author Robin Christ
  */
-public class DisplayController {
+public class DisplayController extends Thread {
+    
+    /**
+     * Last recorded System time the sensor state was high, in milliseconds.
+     */
+    private long highTime;
+    
+    /**
+     * Time until display is set to standby, in milliseconds.
+     */
+    private long STANDBY_TIME = 5000000;
+    
+    /**
+     * Whether exceedance of standby time should be checked.
+     */
+    private boolean bool;
 
     public DisplayController() {
-
+        bool = true;
+        highTime = System.currentTimeMillis();
+        
+        System.out.println("DisplayController initialized2");
     }
 
-    public void test() {
-
-//        GpioController gpio = GpioFactory.getInstance();
-//        
-//        GpioPinDigitalInput input = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07);
-//        input.setMode(PinMode.DIGITAL_INPUT);
-//Create gpio controller for PIR Motion Sensor listening on the pin GPIO_04        
+    @Override
+    public void run() {
         final GpioController gpioPIRMotionSensor = GpioFactory.getInstance();
         final GpioPinDigitalInput pirMotionsensor = gpioPIRMotionSensor.provisionDigitalInputPin(RaspiPin.GPIO_07, PinPullResistance.PULL_DOWN);
-
-        //Create and register gpio pin listener on PIRMotion Sensor GPIO Input instance            
+               
         pirMotionsensor.addListener(new GpioPinListenerDigital() {
 
+            @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                //if the event state is High then print "Intruder Detected" and turn the LED ON by invoking the high() method
                 if (event.getState().isHigh()) {
-                    System.out.println("Intruder Detected!");
+                    System.out.println("Movement detected! " + new Date().toString());
+                    
+                    turnOn();
+                    
+                    bool = false;
                 }
-                //if the event state is Low then print "All is quiet.." and make the LED OFF by invoking the low() method
+                
                 if (event.getState().isLow()) {
-                    System.out.println("All is quiet...");
+                    System.out.println("Ready...");
+                    
+                    bool = true;
+                    
+                    highTime = System.currentTimeMillis(); //time until standby is reset after movement detection
                 }
             }
         });
 
         try {
-            // keep program running until user aborts 
             while (true) {
+                
+                if(bool) {
+                    if(System.currentTimeMillis() - highTime > STANDBY_TIME) {
+                        standby();
+                        bool = false;
+                    }
+                }
+                
                 Thread.sleep(500);  
             }
-        } catch (final Exception e) {
+        } catch (InterruptedException e) {
             System.out.println(e.getMessage());
+        }
+    }
+    
+    /**
+     * Tells a Linux system to turn on the display.
+     */
+    public void turnOn() {
+        try {
+            Runtime.getRuntime().exec("xset dpms force on");
+        } catch (IOException ex) {
+        }
+    }
+    
+    /**
+     * Tells a Linux system to turn off the display.
+     */
+    public void standby() {
+        try {
+            Runtime.getRuntime().exec("xset dpms force standby");
+        } catch (IOException ex) {
         }
     }
 }
--- src/main/DisplayTestMain.java
+++ src/main/DisplayTestMain.java
@@ -20,8 +20,7 @@ public class DisplayTestMain {
      */
     public static void main(String[] args) {
         DisplayController dc = new DisplayController();
-        
-        dc.test();
+        dc.start();
     }
     
 }
