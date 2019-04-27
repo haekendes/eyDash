e36fa67477e9db2a4030594e05bf36e1f6555d6e
Haekendes <haekendes@gmx.net>
2019-04-27 11:23:06 +0200

    Initial commit

---
+++ src/main/DisplayTestMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import DisplayController.DisplayController;
+
+
+
+/**
+ *
+ * @author Robin Christ
+ */
+public class DisplayTestMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DisplayController dc = new DisplayController();
+        
+        dc.test();
+    }
+    
+}
