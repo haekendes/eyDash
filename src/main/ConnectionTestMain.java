e39179472ef7ab8db8dfb8e5bffe55bd0ff63da5
Haekendes <haekendes@gmx.net>
2019-05-07 17:07:13 +0200

    Bluetooth Suche nun vollständig.

--- src/main/ConnectionTestMain.java
+++ src/main/ConnectionTestMain.java
@@ -5,11 +5,7 @@
  */
 package main;
 
-import java.io.IOException;
-import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.bluetooth.*;
+import controls.ConnectionManager;
 
 /**
  *
@@ -17,57 +13,12 @@ import javax.bluetooth.*;
  */
 public class ConnectionTestMain {
 
-        public static final Vector/*<RemoteDevice>*/ devicesDiscovered = new Vector();
-    
+//        public static final Vector/*<RemoteDevice>*/ devicesDiscovered = new Vector();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        final Object inquiryCompletedEvent = new Object();
 
-        devicesDiscovered.clear();
-
-        DiscoveryListener listener = new DiscoveryListener() {
-
-            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
-                devicesDiscovered.addElement(btDevice);
-                try {
-                    System.out.println("     name " + btDevice.getFriendlyName(false));
-                } catch (IOException cantGetDeviceName) {
-                }
-            }
-
-            public void inquiryCompleted(int discType) {
-                System.out.println("Device Inquiry completed!");
-                synchronized(inquiryCompletedEvent){
-                    inquiryCompletedEvent.notifyAll();
-                }
-            }
-
-            public void serviceSearchCompleted(int transID, int respCode) {
-            }
-
-            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-            }
-        };
-
-        synchronized(inquiryCompletedEvent) {
-            boolean started = false;
-            try {
-                started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
-            } catch (BluetoothStateException ex) {
-                Logger.getLogger(ConnectionTestMain.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            if (started) {
-                System.out.println("wait for device inquiry to complete...");
-                try {
-                    inquiryCompletedEvent.wait();
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ConnectionTestMain.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                System.out.println(devicesDiscovered.size() +  " device(s) found");
-            }
-        }
+        new Thread(new ConnectionManager()).start();
     }
 }
